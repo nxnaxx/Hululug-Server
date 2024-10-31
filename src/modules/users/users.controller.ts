98a36583bdf65d96bd12dffce4b51e66d106b3db
programmerJuni <tjrwns2715@naver.com>
2024-11-01 05:47:47 +0900

    feat: possible to signup again with same email after signoff

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -26,6 +26,7 @@ import {
 import { AuthGuard } from '@auth/auth.guard';
 import { Request, Response } from 'express';
 import { AuthService } from '@auth/auth.service';
+import { User } from './schemas';
 
 @Controller('users')
 export class UsersController {
@@ -52,12 +53,11 @@ export class UsersController {
     @Res({ passthrough: true }) res: Response,
   ) {
     const { email, access_token } = await this.signUpService.getKakaoUser(code);
-    const emailCheck = await this.signUpService.emailCheck(email);
-    if (!emailCheck) {
-      throw new NotFoundException('존재하지 않는 사용자입니다');
-    }
 
     const updatedUser = await this.signInService.saveToken(email, access_token);
+    if (!updatedUser) {
+      throw new NotFoundException('존재하지 않는 사용자입니다');
+    }
 
     // 쿠키 생성
     const { accessToken } = await this.authService.getJwtToken(
@@ -92,28 +92,43 @@ export class UsersController {
     @Body() createUserDto: CreateUserDto,
     @Res({ passthrough: true }) res: Response,
   ) {
-    const { code, nickname } = createUserDto;
+    const { nickname, introduce, code } = createUserDto;
 
-    const nicknameCheck = await this.signUpService.nicknameCheck(nickname);
+    const { email, access_token } = await this.signUpService.getKakaoUser(code);
+
+    const nicknameCheck = await this.signUpService.nicknameCheck(
+      email,
+      nickname,
+    );
     if (nicknameCheck) {
       throw new ConflictException('이미 사용하고 있는 닉네임입니다');
     }
 
-    const { email, access_token } = await this.signUpService.getKakaoUser(code);
-
     const emailCheck = await this.signUpService.emailCheck(email);
-    if (emailCheck) {
+    if (emailCheck === 0) {
       throw new ConflictException('이미 존재하는 사용자입니다');
     }
 
     const image = await this.signUpService.uploadImage(profile_image);
 
-    const updatedUser = await this.signUpService.createUser(
-      email,
-      image,
-      access_token,
-      createUserDto,
-    );
+    let updatedUser: User;
+
+    if (emailCheck === 1) {
+      updatedUser = await this.signUpService.createUser(
+        email,
+        image,
+        access_token,
+        createUserDto,
+      );
+    } else {
+      updatedUser = await this.signUpService.updateUser(
+        email,
+        nickname,
+        introduce,
+        image,
+        access_token,
+      );
+    }
 
     // 쿠키 생성
     const { accessToken } = await this.authService.getJwtToken(
@@ -164,7 +179,10 @@ export class UsersController {
     const { nickname, introduce } = updateUserDto;
     const id = req.user.userId;
 
-    const nicknameCheck = await this.signUpService.nicknameCheck(nickname);
+    const nicknameCheck = await this.updateUserService.nicknameCheck(
+      id,
+      nickname,
+    );
     if (nicknameCheck) {
       throw new ConflictException('이미 사용하고 있는 닉네임입니다');
     }
