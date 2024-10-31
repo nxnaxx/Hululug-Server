14a38a4f95f742dbf4c69e156570cb50be07b8ac
programmerJuni <tjrwns2715@naver.com>
2024-10-31 17:02:09 +0900

    feat: add signoff api

--- src/modules/users/services/signoff.service.ts
+++ src/modules/users/services/signoff.service.ts
@@ -8,17 +8,17 @@ import {
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from '../schemas';
 import { Model } from 'mongoose';
-import { firstValueFrom } from 'rxjs';
 import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
-export class SignOutService {
+export class SignOffService {
   constructor(
     private httpService: HttpService,
     @InjectModel(User.name) private userModel: Model<User>,
   ) {}
 
-  async signOut(id: string) {
+  async signOff(id: string) {
     const user = await this.userModel.findById(id);
     if (!user) {
       throw new NotFoundException('사용자를 찾을 수 없습니다.');
@@ -28,25 +28,34 @@ export class SignOutService {
       throw new BadRequestException('액세스 토큰이 없습니다.');
     }
 
-    await this.logoutKakaoAccount(accessToken);
+    await this.unlinkKakaoAccount(accessToken);
     await this.userModel.findOneAndUpdate(
       { _id: id },
-      { access_token: '' },
+      {
+        access_token: '',
+        is_deleted: true,
+        bookmark: [],
+        my_recipes: [],
+        my_comments: [],
+        likes: [],
+      },
       { new: true },
     );
   }
 
-  private async logoutKakaoAccount(accessToken: string): Promise<void> {
-    const logoutUrl = 'https://kapi.kakao.com/v1/user/logout';
-    const headers = {
-      Authorization: `Bearer ${accessToken}`,
-    };
+  private async unlinkKakaoAccount(accessToken: string): Promise<void> {
+    const adminKey = process.env.KAKAO_ADMIN_KEY;
+    const url = 'https://kapi.kakao.com/v1/user/unlink';
 
     try {
-      await firstValueFrom(this.httpService.post(logoutUrl, null, { headers }));
+      await firstValueFrom(
+        this.httpService.post(url, null, {
+          headers: { Authorization: `Bearer ${accessToken}` },
+        }),
+      );
     } catch (error) {
       throw new HttpException(
-        '카카오 로그아웃 실패',
+        '카카오 연결 해제 실패',
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
