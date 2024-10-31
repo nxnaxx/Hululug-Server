fb82767017290922209244d7782d2f10abdc94be
nxnaxx <nxnaxx125@gmail.com>
2024-10-31 23:11:50 +0900

    feat: add cursor-based pagination to recipes API

--- src/modules/recipes/recipes.module.ts
+++ src/modules/recipes/recipes.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from '@auth/auth.module';
-import { UsersModule } from '@modules/users/users.module';
 import { RecipePreview, PreviewSchema } from './recipes.schema';
 import { RecipesController } from './recipes.controller';
 import { RecipesService } from './recipes.service';
@@ -15,10 +14,9 @@ import { User, UserSchema } from '@modules/users/schemas';
     ]),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     AuthModule,
-    UsersModule,
   ],
   controllers: [RecipesController],
   providers: [RecipesService, RecipeMongoRepository],
-  exports: [RecipesService, RecipeMongoRepository],
+  exports: [RecipesService],
 })
 export class RecipesModule {}
--- src/modules/recipes/recipes.service.ts
+++ src/modules/recipes/recipes.service.ts
@@ -1,60 +1,110 @@
 import { Injectable } from '@nestjs/common';
-import { Types } from 'mongoose';
-import { UserId } from '@common/decorators';
-import { FiltersDto } from './dto/filters.dto';
 import { RecipePreview } from './recipes.schema';
 import { RecipeMongoRepository } from './recipes.repository';
+import { GetRecipesDto, SearchRecipesDto } from './dto/get-recipes.dto';
+import { PaginationRecipesDto, ResRecipesDto } from './dto/res-recipes.dto';
 import { decodeQueryParam, stringToObjectId } from 'src/utils';
 
 @Injectable()
 export class RecipesService {
   constructor(private recipeRepository: RecipeMongoRepository) {}
 
-  private isRecipeBookmarked(recipe: RecipePreview, userId: UserId): boolean {
-    if (!userId) return false;
-    return recipe.bookmarks.some(
-      (bookmark) => bookmark.toString() === stringToObjectId(userId).toString(),
-    );
-  }
-
-  private async createPreviewData(recipes: RecipePreview[], userId: UserId) {
+  private async addWriterInfo(
+    recipes: RecipePreview[],
+  ): Promise<ResRecipesDto[]> {
     const userPromises = recipes.map(async (recipe) => {
-      const { bookmarks, writer, ...rest } = recipe;
-      const user = await this.recipeRepository.findUser(writer);
-
-      return {
-        ...rest,
-        writer: user,
-        is_bookmarked: this.isRecipeBookmarked(recipe, userId),
-      };
+      const { writer, ...rest } = recipe;
+      const writerInfo = await this.recipeRepository.findUser(writer);
+      return { ...rest, writer: writerInfo };
     });
-
     return Promise.all(userPromises);
   }
 
-  async getAllRecipes(userId: UserId, filters: FiltersDto) {
-    const { tag, sort } = filters;
-    const recipes = await this.recipeRepository.filteredRecipes(tag);
-
-    recipes.sort((a, b) => {
-      if (sort === 'oldest')
-        return a.created_at.getTime() - b.created_at.getTime();
-      else if (sort === 'popular') return b.likes - a.likes;
-      return b.created_at.getTime() - a.created_at.getTime();
-    });
-
-    const newRecipeData = await this.createPreviewData(recipes, userId);
-
-    return { recipes: newRecipeData };
+  private generateNextCursor(
+    sort: string,
+    paginatedRecipes: ResRecipesDto[],
+  ): string {
+    const lastRecipe = paginatedRecipes[paginatedRecipes.length - 1];
+    return sort === 'popular'
+      ? `${lastRecipe.likes}_${lastRecipe.created_at.getTime()}`
+      : String(lastRecipe.created_at.getTime());
   }
 
-  async searchRecipes(userId: UserId, title: string) {
-    const recipes = await this.recipeRepository.findRecipes(
-      decodeQueryParam(title),
+  async getRecipes(query: GetRecipesDto): Promise<PaginationRecipesDto> {
+    const { limit, tag, sort, cursor } = query;
+    let dbQuery = {};
+    let sortOption = {};
+    let cursorQuery = {};
+
+    if (tag) dbQuery = { tags: stringToObjectId(tag) };
+
+    if (sort === 'popular') {
+      sortOption = { likes: -1, created_at: -1 };
+      if (cursor) {
+        const [likesCursor, createdAtCursor] = cursor.split('_');
+        cursorQuery = {
+          $or: [
+            { likes: { $lt: parseInt(likesCursor, 10) } }, // 좋아요 수가 cursor보다 적거나
+            {
+              likes: parseInt(likesCursor, 10),
+              created_at: { $lt: new Date(parseInt(createdAtCursor, 10)) }, // 좋아요 수가 cursor와 같고 날짜가 cursor 이전
+            },
+          ],
+        };
+      }
+    } else if (sort === 'oldest') {
+      sortOption = { created_at: 1 };
+      if (cursor) {
+        cursorQuery = { created_at: { $gt: new Date(parseInt(cursor, 10)) } }; // cursor 날짜보다 이후
+      }
+    } else {
+      sortOption = { created_at: -1 };
+      if (cursor) {
+        cursorQuery = { created_at: { $lt: new Date(parseInt(cursor, 10)) } }; // cursor 날짜보다 이전
+      }
+    }
+
+    dbQuery = { ...dbQuery, ...cursorQuery };
+
+    const recipes = await this.recipeRepository.filteredRecipes(
+      dbQuery,
+      sortOption,
+      limit,
     );
 
-    const newRecipeData = await this.createPreviewData(recipes, userId);
+    const hasNextPage = recipes.length > limit;
+    const paginatedRecipes = await this.addWriterInfo(
+      hasNextPage ? recipes.slice(0, -1) : recipes,
+    ); // 페이지네이션 시 전달할 데이터 (next cursor가 포함되어 있기 때문에 마지막 요소 제외)
+    const nextCursor = hasNextPage
+      ? this.generateNextCursor(sort, paginatedRecipes)
+      : null;
 
-    return { recipes: newRecipeData };
+    return { recipes: paginatedRecipes, next_cursor: nextCursor };
+  }
+
+  async searchRecipes(query: SearchRecipesDto): Promise<PaginationRecipesDto> {
+    const { limit, keyword, cursor } = query;
+    let cursorQuery = {};
+
+    if (cursor) {
+      cursorQuery = { created_at: { $lt: new Date(parseInt(cursor, 10)) } }; // cursor 날짜보다 이전
+    }
+
+    const recipes = await this.recipeRepository.findRecipes(
+      decodeQueryParam(keyword),
+      cursorQuery,
+      limit,
+    );
+
+    const hasNextPage = recipes.length > limit;
+    const paginatedRecipes = await this.addWriterInfo(
+      hasNextPage ? recipes.slice(0, -1) : recipes,
+    );
+    const nextCursor = hasNextPage
+      ? this.generateNextCursor('newest', paginatedRecipes)
+      : null;
+
+    return { recipes: paginatedRecipes, next_cursor: nextCursor };
   }
 }
