68a1253858b640a5cfb8d20c4d84cf56fe03b1dd
nxnaxx <nxnaxx125@gmail.com>
2024-11-03 06:21:15 +0900

    feat: update response format for create comment API

--- src/modules/comments/comments.repository.ts
+++ src/modules/comments/comments.repository.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
-import { Comment } from './schema/comment.schema';
+import { Comment, CommentDocument } from './schema/comment.schema';
 import { User } from '@modules/users/schemas';
 import { Recipe, RecipeRepository } from '@modules/recipes';
 import { UserId } from '@common/decorators';
+import { WriterDto } from '@modules/recipes/dto';
 
 @Injectable()
 export class CommentRepository {
@@ -15,15 +16,15 @@ export class CommentRepository {
     private readonly recipeRepository: RecipeRepository,
   ) {}
 
-  async saveComment(data: Comment): Promise<Types.ObjectId> {
+  async saveComment(data: Comment): Promise<CommentDocument> {
     // 레시피 존재하는지 확인
     await this.recipeRepository.checkRecipeExists(data.recipe_id);
 
-    const commentId = (await new this.commentModel(data).save())._id;
+    const comment = await new this.commentModel(data).save();
     await this.recipeModel.findByIdAndUpdate(data.recipe_id, {
-      $push: { comments: commentId },
+      $push: { comments: comment._id },
     });
-    return commentId;
+    return comment;
   }
 
   async saveMyComment(
@@ -33,6 +34,14 @@ export class CommentRepository {
     await this.userModel.findByIdAndUpdate(userId, {
       $push: { my_comments: commentId },
     });
+
     return;
+  }
+
+  async findWriter(userId: Types.ObjectId): Promise<WriterDto> {
+    return await this.userModel
+      .findOne({ _id: userId }, { _id: 0, nickname: 1, profile_image: 1 })
+      .lean()
+      .exec();
   }
 }
